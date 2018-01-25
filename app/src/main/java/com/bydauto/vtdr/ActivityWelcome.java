2f28e498cb0c85e8a9cf3cb4c8b7fe4031f427a1
byd_tw <1214658495@qq.com>
2018-01-25 16:36:54 +0800

    update 包名、工程名

---
+++ app/src/main/java/com/bydauto/vtdr/ActivityWelcome.java
@@ -0,0 +1,33 @@
+package com.bydauto.vtdr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * @author byd_tw
+ * @date 2017/12/21
+ */
+
+public class ActivityWelcome extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: 2017/12/21 firstLanuch
+        setContentView(R.layout.layout_welcome);
+        startMainActivity();
+    }
+
+    private void startMainActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ActivityWelcome.this,MainActivity.class));
+                ActivityWelcome.this.finish();
+            }
+        },600);
+    }
+}
